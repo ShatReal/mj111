be9ccf5269235a7095f781969ee15b280a35978c
Emily <tweetychrip@gmail.com>
2022-07-22 11:27:05 -0700

    added catching assets

--- minigames/catching/falling_object.gd
+++ minigames/catching/falling_object.gd
@@ -2,7 +2,20 @@ extends Area2D
 
 
 export var speed := 200.0
-export var rot_speed := 6.0
+export var rot_speed := 3.0
+
+
+func _ready() -> void:
+	var num := randi() % 3
+	if randi() == 0:
+		$Sprite.texture = load("res://minigames/catching/gift1.png")
+		$CollisionShape2D.disabled = false
+	elif randi() == 1:
+		$Sprite.texture = load("res://minigames/catching/gift2.png")
+		$CollisionShape2D2.disabled = false
+	else:
+		$Sprite.texture = load("res://minigames/catching/gift3.png")
+		$CollisionShape2D3.disabled = false
 
 
 func _physics_process(delta: float) -> void:
