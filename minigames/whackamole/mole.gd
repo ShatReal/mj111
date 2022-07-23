e1470cf444e52ad86db08740ec6c0039805b9269
Emily <tweetychrip@gmail.com>
2022-07-22 17:23:28 -0700

    add whackamole

---
+++ minigames/whackamole/mole.gd
@@ -0,0 +1,39 @@
+extends TextureButton
+
+
+signal point(add)
+
+var is_angel := true
+
+
+func _ready() -> void:
+	rect_position = Vector2(-100, -100)
+	if randi() % 2:
+		is_angel = false
+		texture_normal = load("res://minigames/whackamole/krampuspuppet.png")
+
+
+func pop() -> void:
+	var speed := 0.5 if GameManager.debuff else 1.0
+	$Tween.interpolate_property(self, "rect_position:y", rect_position.y, rect_position.y - 100, speed)
+	$Tween.interpolate_property(self, "rect_position:y", rect_position.y - 100, rect_position.y, speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, speed)
+	$Tween.start()
+
+
+func _on_Tween_tween_all_completed() -> void:
+	emit_signal("point", false)
+	queue_free()
+
+
+func _on_Mole_pressed() -> void:
+	emit_signal("point", is_angel)
+	$Tween.stop_all()
+	if is_angel:
+		texture_normal = load("res://minigames/whackamole/nicewhack.png")
+	else:
+		texture_normal = load("res://minigames/whackamole/badwhack.png")
+	$Timer.start()
+
+
+func _on_Timer_timeout() -> void:
+	queue_free()
--- minigames/whackamole/whackamole.gd
+++ minigames/whackamole/whackamole.gd
@@ -1,9 +1,34 @@
 extends Node2D
 
 
-func _ready():
-	yield(SceneManager, "transition_finished")
-	GameManager.load_new_game()
+export var Mole: PackedScene
+
+var points := 0
+
 
 func debuff():
-	print("debuffed!")
+	pass
+
+
+func _on_MoleTimer_timeout() -> void:
+	var m := Mole.instance()
+	$Spots.get_child(randi() % $Spots.get_child_count()).add_child(m)
+	m.connect("point", self, "on_point")
+	m.pop()
+
+
+func on_point(add: bool) -> void:
+	if add:
+		if GameManager.debuff:
+			points += 2
+		else:
+			points += 1
+	else:
+		if GameManager.debuff:
+			points -= 2
+		else:
+			points -= 1
+
+
+func _on_GameTimer_timeout() -> void:
+	GameManager.finish_game(points)
