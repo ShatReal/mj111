40a6fb2030090abe18f6d3a38f31239fd0c1217d
realjavabot <realtalkingseed@gmail.com>
2022-07-23 12:44:37 -0700

    added backgrounds and made the dice game 3d

--- minigames/dice_roll/dice_roll.gd
+++ minigames/dice_roll/dice_roll.gd
@@ -1,27 +1,43 @@
 extends Control
 
+enum state {GOOD, BAD}
+onready var die = $ViewportContainer/Viewport/die
+onready var raycasts = $ViewportContainer/Viewport/die/raycasts
+var faces = [state.GOOD, state.GOOD, state.BAD, state.GOOD, state.BAD, state.GOOD]
+var award = 20
+onready var debuff_mat = load("res://minigames/dice_roll/debuff_die.material")
 
-var points := 0
-
-func debuff() -> void:
-	pass
-
-
-func _on_Timer_timeout() -> void:
-	$VBoxContainer/Num.text = str(randi() % 98 + 1)
-
-
-func _on_Timer2_timeout() -> void:
-	$Timer.stop()
-	var points = 0
-	if GameManager.debuff:
-		points = randi() % 25 - 5
-	else:
-		points = randi() % 10 + 1
-	$VBoxContainer/Num.text = str(points)
-	GameManager.earn_points(points)
+func _ready():
+	$endtimer.start()
+	$Timer2.start()
 	$Timer3.start()
 
+func debuff() -> void:
+	award = 30
+	faces = [state.BAD, state.GOOD, state.GOOD, state.GOOD, state.BAD, state.BAD]
+	die.get_node("die_mesh/Cube").mesh.surface_set_material(0, debuff_mat)
+
+func _on_Timer2_timeout() -> void:
+	die.apply_central_impulse(Vector3.UP * 5)
+	die.apply_torque_impulse(RandUtils.vec3() * 8)
 
 func _on_Timer3_timeout() -> void:
+	var index = 0
+	var largest = 0
+	var largest_index = 0
+	
+	for raycast in raycasts.get_children():
+		var normal = Vector3.UP.dot(raycast.get_collision_normal())
+		if normal > largest:
+			largest = normal
+			largest_index = index
+		index += 1
+		
+	if faces[largest_index] == state.GOOD:
+		GameManager.earn_points(award)
+	else:
+		GameManager.earn_points(-award)
+
+
+func _on_endtimer_timeout():
 	GameManager.finish_game()
